9a3a9d43d786c21ef71b6c2d1c0719c0f8d1b933
Slawomir Johansen <baran@fastmail.com>
2022-02-01 16:56:16 +0100

    Add script.js computer logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-let playerMove = 'paper';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Random number equals: ' + randomNumber);
+
+let computerMove = 'unknown move';
+
+if(randomNumber == 1){
+  computerMove = 'rock';
+} else if (randomNumber == 2){
+    computerMove = 'paper';
+}
+else (randomNumber == 3){
+    computerMove = 'scissor'
+}
+
+
+printMessage('Mój ruch to: ' + computerMove);
