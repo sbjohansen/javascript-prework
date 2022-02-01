a8238f846a6760913743d77e29aecd1f8acdbfd7
Slawomir Johansen <baran@fastmail.com>
2022-02-01 20:25:01 +0100

    Add function displayResult & logic for it

--- js/script.js
+++ js/script.js
@@ -1,49 +1,49 @@
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'rock';
+    }
+    else if(argMoveId == 2){
+        return 'paper';
+    }
+    else if(argMoveId == 3){
+        return 'scissor';
+    } else { printMessage('I cant recognize move with id ' + argMoveId + '.');
+    return 'unknown move';
+    }  
+}
+
+function displayResult(argComputerMove, argPlayerMove){
+    
+    
+    printMessage('Computer played ' + '<strong>' + argComputerMove + '</strong>' + ', player played ' + '<strong>' + argPlayerMove + '</strong>.');
+    
+    if( argComputerMove == 'rock' && argPlayerMove == 'paper'){
+        printMessage('Player won!');
+    } else if( argComputerMove == 'paper' && argPlayerMove == 'scissor'){
+          printMessage('Player won!');
+    } else if( argComputerMove == 'scissor' && argPlayerMove == 'rock'){
+          printMessage('Player won!');    
+    } else if( argComputerMove == argPlayerMove) {
+          printMessage('Tie');
+    } else if( argComputerMove == 'unknown move') {
+            printMessage('Stop cheating!');
+    } else if( argPlayerMove == 'unknown move') {
+            printMessage('Someone is trying to cheat!');
+    } else { printMessage('Computer Won');
+    }
+}
+
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Random number equals: ' + randomNumber);
 
-let computerMove = 'unknown move';
-
-if(randomNumber == 1){
-  computerMove = 'rock';
-} else if (randomNumber == 2){
-    computerMove = 'paper';
-}
-else (randomNumber == 3){
-    computerMove = 'scissor'
-}
-
-
-printMessage('My move is: ' + computerMove);
+let argComputerMove = getMoveName(randomNumber);
 
 let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissor.');
 
 console.log('Player played: ' + playerInput);
 
-let playerMove = 'unknown move';
+let argPlayerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
-  playerMove = 'rock';
-} else if(playerInput == '2'){
-    playerMove = 'paper';
-} else if(playerInput == '3'){
-    playerMove = 'scissor';
-}
-
-printMessage('Your move is: ' + playerMove);
-
-if( computerMove == 'rock' && playerMove == 'paper'){
-    printMessage('You won!');
-  } else if( computerMove == 'paper' && playerMove == 'scissor'){
-      printMessage('You won!');
-  } else if( computerMove == 'scissor' && playerMove == 'rock'){
-      printMessage('You won!');    
-  } else if( playerMove == 'unknown move') {
-      printMessage('Something went wrong, try again');
-  } else if(computerMove && playerMove == 'rock'){
-      printMessage('Tie');
-  } else if( computerMove && playerMove == 'paper'){
-      printMessage('Tie');
-  } else if( computerMove && playerMove == 'scissor'){
-      printMessage('Tie');
-  } else( printMessage('Computer won!'))
+displayResult(argComputerMove, argPlayerMove);
